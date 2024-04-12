6498085c2b6b1b3f31e2f14830b92aeb3b66b870
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2024-04-12 10:32:59 +0200

    Implement authentication APIs

--- backend/src/main/java/org/latexscribe/LatexScribe/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/org/latexscribe/LatexScribe/config/JwtAuthenticationFilter.java
@@ -6,27 +6,62 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.latexscribe.LatexScribe.service.IJwtService;
+import org.latexscribe.LatexScribe.service.ITokenService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private static final String AUTHORIZATION_PREFIX = "Bearer ";
 
-    private final IJwtService jwtService;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    private final ITokenService tokenService;
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
-        if (authHeader == null || !authHeader.startsWith(AUTHORIZATION_PREFIX)) {
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        if (request.getServletPath().contains("/api/v1/auth")) {
             filterChain.doFilter(request, response);
             return;
         }
-
-        final String jwt = authHeader.substring(AUTHORIZATION_PREFIX.length());
-
-        final String username = jwtService.extractUsername(jwt);
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null ||!authHeader.startsWith(AUTHORIZATION_PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(AUTHORIZATION_PREFIX.length());
+        userEmail = jwtService.extractUsername(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            var isTokenValid = tokenService.findByToken(jwt)
+                    .map(t -> !t.isExpired() && !t.isRevoked())
+                    .orElse(false);
+            if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
---
+++ backend/src/main/java/org/latexscribe/LatexScribe/webcontroller/AuthController.java
@@ -0,0 +1,38 @@
+package org.latexscribe.LatexScribe.webcontroller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.latexscribe.LatexScribe.domain.dto.AuthRequestDto;
+import org.latexscribe.LatexScribe.domain.dto.AuthResponseDto;
+import org.latexscribe.LatexScribe.domain.dto.RegisterRequestDto;
+import org.latexscribe.LatexScribe.service.IAuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final IAuthService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDto> register(@RequestBody RegisterRequestDto request) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> authenticate(@RequestBody AuthRequestDto request) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        service.refreshToken(request, response);
+    }
+}
