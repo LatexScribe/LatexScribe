3a12e272a80e6387cc617b3d247cfd79028e12f5
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2024-04-12 07:45:04 +0200

    Implement user menagement

--- backend/src/main/java/org/latexscribe/LatexScribe/domain/model/User.java
+++ backend/src/main/java/org/latexscribe/LatexScribe/domain/model/User.java
@@ -1,19 +1,23 @@
 package org.latexscribe.LatexScribe.domain.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import org.latexscribe.LatexScribe.domain.enums.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "auth_user")
 @RequiredArgsConstructor
-public class User {
+@Builder
+@AllArgsConstructor
+public class User implements UserDetails {
     @Id
     private String username;
 
@@ -23,4 +27,42 @@ public class User {
     private String fullName;
 
     private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ backend/src/main/java/org/latexscribe/LatexScribe/service/impl/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package org.latexscribe.LatexScribe.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.latexscribe.LatexScribe.domain.model.User;
+import org.latexscribe.LatexScribe.repository.UserRepository;
+import org.latexscribe.LatexScribe.service.IUserService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements IUserService  {
+    private final UserRepository userRepository;
+
+    @Override
+    public void save(User user) {
+        if (user == null) {
+            throw new IllegalArgumentException("provided user is null");
+        }
+        userRepository.saveAndFlush(user);
+    }
+
+    @Override
+    public void delete(User user) {
+        if (user == null) {
+            throw new IllegalArgumentException("provided user is null");
+        }
+        userRepository.delete(user);
+    }
+
+    @Override
+    public void deleteByUsername(String username) {
+        if (username == null) {
+            throw new IllegalArgumentException("provided user username is null");
+        }
+        userRepository.deleteById(username);
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
