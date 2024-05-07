5561d09e51ed2383e245c23ff15d4a9b0b0af4c3
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2024-05-07 20:01:53 +0200

    Remove trailing slashes from API end-points (#18)

--- backend/src/main/java/org/latexscribe/LatexScribe/webcontroller/TemplateController.java
+++ backend/src/main/java/org/latexscribe/LatexScribe/webcontroller/TemplateController.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 public class TemplateController {
     private final ITemplateService templateService;
 
-    @GetMapping("/")
+    @GetMapping
     public List<DocumentTemplate> listAll() {
         return templateService.listAll();
     }
