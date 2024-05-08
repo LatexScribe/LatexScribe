a1889a7631db2e8d643514ed5ac841671dbcdf97
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2024-05-08 14:40:04 +0200

    Use filter to handle CROS errors (#19)

--- backend/src/main/java/org/latexscribe/LatexScribe/webcontroller/TagController.java
+++ backend/src/main/java/org/latexscribe/LatexScribe/webcontroller/TagController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/tags")
 public class TagController {
--- backend/src/main/java/org/latexscribe/LatexScribe/webcontroller/TemplateController.java
+++ backend/src/main/java/org/latexscribe/LatexScribe/webcontroller/TemplateController.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/templates")
 public class TemplateController {
