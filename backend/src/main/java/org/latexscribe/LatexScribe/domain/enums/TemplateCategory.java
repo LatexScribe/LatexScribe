722e213f6aeb1da17518a5356dabcacd3191f776
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2024-05-11 05:58:06 +0200

    Merge `/templates` and `/templates/category` API endpoints (#23)

--- backend/src/main/java/org/latexscribe/LatexScribe/domain/enums/TemplateCategory.java
+++ backend/src/main/java/org/latexscribe/LatexScribe/domain/enums/TemplateCategory.java
@@ -1,4 +1,16 @@
 package org.latexscribe.LatexScribe.domain.enums;
 
 public enum TemplateCategory {
+    ACADEMIC_JOURNAL,
+    BIBLIOGRAPHY,
+    BOOK,
+    CALENDER,
+    RESUME,
+    FORMAL_LETTER,
+    HOMEWORK_ASSIGNMENT,
+    NEWSLETTER,
+    POSTER,
+    PRESENTATION,
+    PROJECT,
+    THESIS,
 }
--- backend/src/main/java/org/latexscribe/LatexScribe/webcontroller/TemplateController.java
+++ backend/src/main/java/org/latexscribe/LatexScribe/webcontroller/TemplateController.java
@@ -7,6 +7,7 @@ import org.latexscribe.LatexScribe.domain.model.DocumentTemplate;
 import org.latexscribe.LatexScribe.domain.model.User;
 import org.latexscribe.LatexScribe.service.ITemplateService;
 import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -20,7 +21,10 @@ public class TemplateController {
     private final ITemplateService templateService;
 
     @GetMapping
-    public List<DocumentTemplate> listAll() {
+    public List<DocumentTemplate> listTemplates(@RequestParam(required = false) TemplateCategory category) {
+        if (category != null) {
+            return templateService.listAllByCategory(category);
+        }
         return templateService.listAll();
     }
 
@@ -28,11 +32,6 @@ public class TemplateController {
 //    public List<DocumentTemplate> listAllByUser(User user) {
 //        return templateService.listAllByUser(user);
 //    }
-
-    @GetMapping("/category")
-    public List<DocumentTemplate> listAllByCategory(@RequestParam TemplateCategory category) {
-        return templateService.listAllByCategory(category);
-    }
 
     @GetMapping("/{id}")
     public DocumentTemplate getById(@PathVariable Long id) {
